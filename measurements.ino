cbe69cd9c89eaf2f9f81be5f44e57396285bb581
Tyraka <42555839+Tyraka@users.noreply.github.com>
2018-09-04 00:09:36 +0200

    Update measurements.ino
    fixed kurwa

--- measurements.ino
+++ measurements.ino
@@ -8,13 +8,10 @@ HX711 scale(DOUT, CLK);
 const float calibration_factor = -96650;
 
 void setup() {
-  // Initialize scale on right pins
-  hx711.init(CLK, DOUT);
-
   scale.set_scale(-96650);  //Calibration adjustment
   scale.tare();             //Reset the scale to 0  
 }
 
 void loop() {  
-  Serial.plrintln(scale.get_units(), 3); // up to 3 decimals
+  Serial.println(scale.get_units(), 3); // up to 3 decimals
 }
